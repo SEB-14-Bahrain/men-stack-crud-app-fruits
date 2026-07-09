efdc338a568f7fe7a302a32715a6a67a04bc56a3
nayaba <nabila.ayaba@gmail.com>
2026-07-09 12:55:49 +0300

    Finds fruits via the filter object

--- server.js
+++ server.js
@@ -21,16 +21,11 @@ app.get('/', async (req, res) => {
 
 // this route will change often
 app.get('/fruits', async (req, res) => {
-    // create a fruit object
-    const fruitData = {}
-    fruitData.name = 'Blueberry'
-    fruitData.isReadyToEat = false
-
-    // use a mongoose method to add it to the DB
-    let createdFruit = await Fruit.create(fruitData)
+    // use a mongoose method to find all the bananas
+    let notReady = await Fruit.find({ isReadyToEat: false})
     
-    // view the created fruit
-    res.send(createdFruit)
+    // view the found fruit
+    res.send(notReady)
 })
 
 app.listen(3000, () => {
@@ -42,6 +37,7 @@ app.listen(3000, () => {
 
 // CODE GRAVEYARD ===============================
 
+// CREATE ONE FRUIT
     // // create a fruit object
     // const fruitData = {}
     // fruitData.name = 'Blueberry'
@@ -49,3 +45,15 @@ app.listen(3000, () => {
 
     // // use a mongoose method to add it to the DB
     // let createdFruit = await Fruit.create(fruitData)
+
+// FIND ALL FRUITS
+        // use a mongoose method to find all the Fruits
+    // let allFruits = await Fruit.find()
+
+// FIND ALL FRUITS WITH name OF Banana
+        // use a mongoose method to find all the bananas
+    // let allBananas = await Fruit.find({ name: 'Banana'})
+
+// FIND ALL FRUITS WHERE isReadyToEat IS false
+        // use a mongoose method to find all fruits where isReadyToEat is false
+    // let notReady = await Fruit.find({ isReadyToEat: false})
